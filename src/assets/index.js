c4549fceb05a35fcf32a9a7fc2f1de1c6ded9df9
yaser <yaseryjjou@gmail.com>
2024-12-04 12:59:09 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,100 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import shaq from './personal/shaq.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import mobile from './icons/mobile_development.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import Cpp from './tech/C++.png';
+import android_studio from './tech/android_studio.png';
+import arduino from './tech/arduino.png';
+import C from './tech/C.png';
+import kotlin from './tech/kotlin.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import python from './tech/python.png';
+import Flutter from './tech/flutter.png';
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+
+import komikult from './projects/komikult.png';
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  shaq,
+  backend,
+  mobile,
+  ux,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  Cpp,
+  postgresql,
+  Flutter,
+  python,
+  android_studio,
+  arduino,
+  C,
+  kotlin,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        I am a passionate Mobile App Developer with experience in building modern and user-friendly Android and iOS applications. 
+        My expertise also extends to Full-Stack Development, creating secure and scalable web solutions, 
+        UI/UX design for intuitive user experiences, and Robotics Prototyping to bring innovative ideas to life. 
+        My goal is to craft technology solutions that not only meet client expectations but exceed them.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default SectionWrapper(About, 'about');
